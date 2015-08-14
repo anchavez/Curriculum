44d9f7081ea3d0f3e1e24a241bb352f2c0a56980
anchavez <anthonycastillochavez@hotmail.com>
2015-08-14 14:30:53 +0000

    Merge remote-tracking branch 'upstream/master'

---
+++ Week_3/Day_2/Challenges/01_very_easy.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+    <p>
+
+        <?php
+            /*
+             * Write a program that loops through $inputs['numbers'] and outputs
+             * each number.
+             *
+             * But for multiples of three print “Fizz” instead of the number
+             * and for the multiples of five print “Buzz”. For numbers which
+             * are multiples of both three and five print “FizzBuzz”
+             *
+             */
+
+             $inputs = array(
+               'vowels' => array('a','e','i','o','u'),
+               'numbers' => range(0, 100)
+             );
+
+        ?>
+
+    </p>
+
+    </body>
+</html>
